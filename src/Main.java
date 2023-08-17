10f8eee7f1c41dabf73095b1df4a9293ea54821d
asheralieva <84007297+asheralieva@users.noreply.github.com>
2023-08-17 13:06:20 +0600

    add text new Date

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Date date = new Date();
+        System.out.println("we add new Date!");
     }
 }
